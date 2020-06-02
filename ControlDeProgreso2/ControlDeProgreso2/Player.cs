3d7856ebc28cbf41cf2566734951347745b01056
HernanOcaranza <hernanocaranza@hotmail.com.ar>
2020-06-02 17:11:41 -0300

    mejorando la clase player

--- ControlDeProgreso2/ControlDeProgreso2/Player.cs
+++ ControlDeProgreso2/ControlDeProgreso2/Player.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace ControlDeProgreso2
 {
+    enum TipoPersonaje
+    {
+        tipo1,
+        tipo2,
+        tipo3
+    };
+
     enum Maximos
     {
         velocidad = 10,
@@ -14,22 +21,49 @@ namespace ControlDeProgreso2
     };
     class Player
     {
-        private string tipo;
+        private TipoPersonaje tipo;
         private string nombre;
         private string apodo;
         private DateTime fechaNac;
         private int salud;
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
 
-        public string Tipo { get => tipo; set => tipo = value; }
+        public Player(string nombreIngresado, string apodoIngresado, DateTime fechNacIngresada)
+        {
+            //Falta el tipo de dato
+            Random NumeroAleatorio = new Random();
+            nombre = nombreIngresado;
+            apodo = apodoIngresado;
+            fechaNac = fechNacIngresada;
+            salud = 100;
+            velocidad = NumeroAleatorio.Next((int)Maximos.velocidad);
+            destreza = NumeroAleatorio.Next((int)Maximos.destreza);
+            fuerza = NumeroAleatorio.Next((int)Maximos.fuerza);
+            nivel = NumeroAleatorio.Next((int)Maximos.nivel);
+            armadura =  NumeroAleatorio.Next((int)Maximos.armadura);
+        }
+
         public string Nombre { get => nombre; set => nombre = value; }
         public string Apodo { get => apodo; set => apodo = value; }
         public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
         public int Salud { get => salud; set => salud = value; }
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+        public TipoPersonaje Tipo { get => tipo; set => tipo = value; }
 
-        /*public int Edad()
+
+        public int Edad()
         {
-            
-        }*/
+            int EdadPlayer = DateTime.Now.Year - FechaNac.Year;
+            return EdadPlayer;
+        }
 
     }
 }
